0c05262c36ac1d1f3b0f92a908e1a61f06a10244
Jesse Miller <silverhonda84@gmail.com>
2025-05-03 08:40:51 -0700

    Update index.js
    sort is working but it is inside the function so it sorts when loading

--- index.js
+++ index.js
@@ -2,27 +2,35 @@ const moviesListEl = document.querySelector(".movies");
 
 function onSearchChange(event) {
     const searchTerm = event.target.value;
-    const filter = filterMovies();
-    renderMovies(searchTerm, filter);
+    renderMovies(searchTerm);
+}
+
+function filterMovies(event) {
+    const selectElement = document.querySelector("#filter");
+    console.log(selectElement.value);
+    return selectElement.value;
 }
 
 function displayMovies(movies) {
     moviesListEl.innerHTML = movies.map(movie => movieHTML(movie)).join('');
 }
 
-async function renderMovies(searchTerm, filter) {
+async function renderMovies(searchTerm) {
+    const filter = filterMovies();
     const response = await fetch(`https://www.omdbapi.com/?s=${searchTerm}&apikey=94c6a856`);
     const moviesData = await response.json();
-    let movies = moviesData.Search || [];
+    let movies = moviesData.Search || []; 
 
-    if (filter === "OLD_TO_NEW") {
-      movies.sort((a, b) => a.Year - b.Year);
-  } else if (filter === "NEW_TO_OLD") {
-      movies.sort((a, b) => b.Year - a.Year);
-  }
-
-    displayMovies(movies);
-    moviesListEl.classList.remove('movies__loading');
+    setTimeout(() => {
+        if (filter === "OLD_TO_NEW") {
+            movies.sort((a, b) => Number(a.Year) - Number(b.Year));
+        } else if (filter === "NEW_TO_OLD") {
+            movies.sort((a, b) => Number(b.Year) - Number(a.Year));
+        }
+        
+        displayMovies(movies);
+        moviesListEl.classList.remove('movies__loading');
+    }, 1000);
 }
 
 function movieHTML(movie) {
@@ -34,11 +42,6 @@ function movieHTML(movie) {
             <div class="movie__title">${movie.Title}</div>
             <div class="movie__price">${movie.Year}</div>
         </div>`;
-}
-
-function filterMovies(event) {
-    const selectElement = document.querySelector("#filter");
-    return selectElement.value;
 }
 
 function openMenu() {
